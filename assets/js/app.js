c839b0200a19ca618cc27939bd2c22cea1edb3af
DataScientist <t_doan2@uncg.edu>
2020-04-02 17:00:33 -0400

    a funny message here

--- assets/js/app.js
+++ assets/js/app.js
@@ -406,3 +406,36 @@ function nationalView() {
         console.log(error);
         });
 };
+
+// start of thien's code
+// by state
+d3.csv("assets/data/unemployment_claims_by_state.csv").then(function(data){
+    console.log(data[0]);
+});
+
+// claims by date
+d3.csv("assets/data/unemployment_claims.csv").then(function(data){
+    console.log(data[0]);
+    var dates = [];
+    var claims = [];
+    for(var i = 0;i<data.length;i++){
+        dates.push(data[i].WeekEnding);
+        claims.push(data[i].InitialClaims);
+    }
+    // console.log(dates);
+    // console.log(claims);
+
+    // chart
+    var trace1 = {
+        x: dates,
+        y: claims,
+        type: 'scatter'
+    };
+    // layout
+    var layout = {
+        width: 500,
+        height: 500
+    };
+    var claims_data = [trace1];
+    Plotly.newPlot('unemployment',claims_data,layout);
+});
